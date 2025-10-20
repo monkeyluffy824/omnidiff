7241dcac1702baf7c042371077b286f535076b12
monkeyluffy824 <138200438+monkeyluffy824@users.noreply.github.com>
2025-10-20 22:27:21 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md","r") as fh:
+	long_description=fh.read()
+    
+setuptools.setup(
+    name="omnidiff",
+    version="0.1.0",
+    author="monkeyluffy824",
+    author_email="panangipallisaicharan@gmail.com",
+    description="A scalar-valued autograd engine, consider this like an extension of micrograd with support of more complex functions",
+    long_description=long_description,
+    url="https://github.com/monkeyluffy824/omnidiff",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
